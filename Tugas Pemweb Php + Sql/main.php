087e230807fdd7ba7a840ae33109fe9ea60976d0
Rantau Himawan <88751108+pharaohpants@users.noreply.github.com>
2023-03-14 19:19:15 +0700

    Add files via upload

---
+++ Tugas Pemweb Php + Sql/main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center><div style="background-color:#666362; top:40%; width:40%;border-radius:100px;">
+    <center><h1>SELAMAT DATANG</h1>
+    <h2>Silahkan Pilih Data Mana yang ditampilkan</h2></center>
+</div></center>
+        <center><table border = "1"> 
+        <tr>
+        <td><center><h3>Jenis Data<h3></td>
+        </tr>
+        <tr>
+        <td><a href="customer.php"><h3>Data Customer</h3></a></td>
+        </tr>
+        <tr>
+        <td> <a href="product.php"><h3>Data Product</h3></a></td>
+        </tr>
+        </table></center>
+
+</body>
+</html>
