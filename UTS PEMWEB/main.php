892ff3d3f834ba77d464a9d7ad7cdbd7cb2ba12d
Rantau Himawan <88751108+pharaohpants@users.noreply.github.com>
2023-04-06 01:32:40 +0700

    Add files via upload

---
+++ UTS PEMWEB/main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body><div><center>
+    <h1>UTS Pemweb</h1>
+    <h3>Nama : Rantau Himawan </h3>
+    <h3>NPM  : 21081010086 </h3>
+    </div></center>
+        <center><table border = "1"> 
+        <tr>
+        <td><center><h3>Jenis Data<h3></td>
+        <td><a href="bus.php"><h3>Data Customer</h3></a></td>
+        <td> <a href="kondektur.php"><h3>Data Kondektur</h3></a></td>
+        <td> <a href="driver.php"><h3>Data Driver</h3></a></td>
+        <td> <a href="transupn.php"><h3>Data Trans UPN</h3></a></td>
+        <td> <a href="gajikondektur.php"><h3>Perhitungan Gaji Kondektur</h3></a></td>
+        <td> <a href="gajidriver.php"><h3>Perhitungan Gaji Driver</h3></a></td>
+        </tr>
+        </table></center>
+</body>
+</html>
